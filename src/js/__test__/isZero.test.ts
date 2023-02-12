a8bc485c8105035867bc26e53ea1ecc46bd2bdd3
koh124 <altkohei212@gmail.com>
2023-02-12 19:58:23 +0900

    テスト環境を更新

--- src/js/__test__/isZero.test.ts
+++ src/js/__test__/isZero.test.ts
@@ -1,4 +1,4 @@
-import { isZero } from '../components/isZero'
+import { isZero } from './isZero'
 
 test('0を渡したらtrueになること', () => {
   const result = isZero(0)
---
+++ src/js/__test__/isZero.ts
@@ -0,0 +1,4 @@
+type TF = 0 | 1
+export const isZero = (num: TF): boolean => {
+  return num === 0
+}
